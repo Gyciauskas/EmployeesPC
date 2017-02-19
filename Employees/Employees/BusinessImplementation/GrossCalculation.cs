305aa9c4f3d618d21d80f6dfe193dadb0608dc8a
Gytis <gyciauskas@hotmail.com>
2017-02-19 11:46:16 +0200

    corrected salary calculation system

--- Employees/Employees/BusinessImplementation/GrossCalculation.cs
+++ Employees/Employees/BusinessImplementation/GrossCalculation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -9,18 +10,20 @@ namespace Employees.BusinessImplementation
     {
         public double Count(double netSalary)
         {
+            double baseNPD = Double.Parse(ConfigurationManager.AppSettings["baseNPD"]);
+            double mmaWage = Double.Parse(ConfigurationManager.AppSettings["mmaWage"]);
             double grossSalary = 0;
-            double incomeTax = 0.15;
-            double healthInsurance = 0.06;
-            double pensionInsurance = 0.03;
+            double incomeTax = Double.Parse(ConfigurationManager.AppSettings["incomeTax"]) / 100;
+            double healthInsurance = Double.Parse(ConfigurationManager.AppSettings["healthInsurance"]) / 100;
+            double pensionInsurance = Double.Parse(ConfigurationManager.AppSettings["pensionInsurance"]) / 100;
 
             if (netSalary <= 335.3)
             {
-                grossSalary = (netSalary - 46.5) / (1 - (incomeTax + healthInsurance + pensionInsurance));
+                grossSalary = (netSalary - (incomeTax * baseNPD)) / (1 - (incomeTax + healthInsurance + pensionInsurance));
             }
-            else if (netSalary < 760)
+            else if (netSalary <= 760)
             {
-                grossSalary = (netSalary - 75) / (1 - ((incomeTax * 1.5) + healthInsurance + pensionInsurance));
+                grossSalary = (netSalary - (0.15 * (baseNPD + (mmaWage / 2)))) / (1 - ((incomeTax * 1.5) + healthInsurance + pensionInsurance));
             }
             else
             {
--- Employees/Employees/Controllers/EmployeesController.cs
+++ Employees/Employees/Controllers/EmployeesController.cs
@@ -5,6 +5,7 @@ using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Employees.Controllers
